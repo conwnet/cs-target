653bce341382c7a3cb64d9b2c647e23fcae7bd74
netcon <netcon@live.com>
2017-04-17 23:52:54 +0800

    version 1.0

---
+++ public/static/index/js/scripts.js
@@ -0,0 +1,32 @@
+
+jQuery(document).ready(function() {
+
+    $('.page-container form').submit(function(){
+        var username = $(this).find('.username').val();
+        var password = $(this).find('.password').val();
+        if(username == '') {
+            $(this).find('.error').fadeOut('fast', function(){
+                $(this).css('top', '27px');
+            });
+            $(this).find('.error').fadeIn('fast', function(){
+                $(this).parent().find('.username').focus();
+            });
+            return false;
+        }
+        if(password == '') {
+            $(this).find('.error').fadeOut('fast', function(){
+                $(this).css('top', '96px');
+            });
+            $(this).find('.error').fadeIn('fast', function(){
+                $(this).parent().find('.password').focus();
+            });
+            return false;
+        }
+    });
+
+    $('.page-container form .username, .page-container form .password').keyup(function(){
+        $(this).parent().find('.error').fadeOut('fast');
+    });
+
+    console.log('我是你们萌萌哒国庆学长，系统弱不禁风，不要做坏事哦~');
+});
